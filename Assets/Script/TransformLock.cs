e0a7770d2b59f638cca34039547a1f6c795998c4
co1umbine <hukutunojyoni@gmail.com>
2020-10-29 18:43:33 +0900

    [add] Kinova Gen3 6dof Arm

---
+++ Assets/Script/TransformLock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RobotSimulation
+{
+    public class TransformLock : MonoBehaviour
+    {
+        Vector3 keepPosition;
+        Quaternion keepRotation;
+        Vector3 keepScale;
+        void Start() 
+        { 
+        
+            keepPosition = transform.position;
+            keepRotation = transform.rotation;
+            keepScale = transform.localScale;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = keepPosition;
+            transform.rotation = keepRotation;
+            transform.localScale = keepScale;
+        }
+    }
+}
