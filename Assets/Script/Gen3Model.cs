29ebb6d3614d8283704f7d3686917f19caf8a35e
co1umbine <co1umbine@users.noreply.github.com>
2020-11-12 20:29:12 +0900

    [update] 11/12 zemi

--- Assets/Script/Gen3Model.cs
+++ Assets/Script/Gen3Model.cs
@@ -13,6 +13,7 @@ namespace RobotSimulation
     {
         [SerializeField] List<JointStateReader> joints;  // 1~6
         List<JointStateWriter> jointsW = new List<JointStateWriter>();
+        List<HingeJoint> hinge = new List<HingeJoint>();
         [SerializeField] Transform[] fowardK;
         HomogeneourCoordinate hC;
         List<Matrix4x4> HTMs = new List<Matrix4x4>();
@@ -25,6 +26,7 @@ namespace RobotSimulation
             foreach(var j in joints)
             {
                 jointsW.Add(j.GetComponent<JointStateWriter>());
+                hinge.Add(j.GetComponent<HingeJoint>());
             }
         }
 
@@ -38,17 +40,14 @@ namespace RobotSimulation
             fowardK[fowardK.Length - 1].position = joints[joints.Count() - 1].transform.position + joints[joints.Count()-1].transform.rotation * new Vector3(hE.x, hE.z, hE.y);
             */
 
-            foreach(var joint in jointsW)
+            foreach (var joint in jointsW)
             {
-                joint.Write(0);
+                joint.Write(0.5f * Mathf.PI);
             }
             var thetas = new List<float>();
-            foreach (var joint in joints)
+            foreach (var joint in hinge)
             {
-                string s;
-                float tmp, v, e;
-                joint.Read(out s, out tmp, out v, out e);
-                thetas.Add(-tmp);
+                thetas.Add(joint.angle);
             }
             print($"angles { thetas[0] * Mathf.Rad2Deg}, { thetas[1] * Mathf.Rad2Deg}, { thetas[2] * Mathf.Rad2Deg}, { thetas[3] * Mathf.Rad2Deg}, { thetas[4] * Mathf.Rad2Deg}, { thetas[5] * Mathf.Rad2Deg}");
             HTMs = hC.GetHTM(thetas);
@@ -58,6 +57,7 @@ namespace RobotSimulation
                 fowardK[i].localPosition = HTMs[i] * new Vector4(0, 0, 0, 1);
             }
             fowardK[fowardK.Length - 1].localPosition = HTMs[HTMs.Count() - 1] * hE;
+            UnityEditor.EditorApplication.isPaused = true;
         }
 
         private void OnDrawGizmos()
@@ -65,16 +65,16 @@ namespace RobotSimulation
             int i = 0;
             foreach(var htm in HTMs)
             {
-                if (i == dispJoint) 
-                {
-                    Gizmos.color = Color.red;
-                    Vector3 origin = htm * new Vector4(0, 0, 0, 1);
-                    Vector3 x1 = htm * new Vector4(0.1f, 0, 0, 1);
-                    Gizmos.DrawLine(origin, x1);
-                    Gizmos.color = Color.blue;
-                    Vector3 z1 = htm * new Vector4(0, 0, 0.1f, 1);
-                    Gizmos.DrawLine(origin, z1); 
-                }
+                //if (i == dispJoint) 
+                //{
+                Gizmos.color = Color.red;
+                Vector3 origin = htm * new Vector4(0, 0, 0, 1);
+                Vector3 x1 = htm * new Vector4(0.1f, 0, 0, 1);
+                Gizmos.DrawLine(origin, x1);
+                Gizmos.color = Color.blue;
+                Vector3 z1 = htm * new Vector4(0, 0, 0.1f, 1);
+                Gizmos.DrawLine(origin, z1); 
+                //}
 
                 i++;
             }
--- Assets/Script/HomogeneourCoordinate.cs
+++ Assets/Script/HomogeneourCoordinate.cs
@@ -21,14 +21,14 @@ namespace RobotSimulation
     {
         public List<LinkParam> LinkParams;
         public Matrix4x4 HTM;
-        private List<float> deltaThetasDeg = new List<float>();
+        public List<float> deltaThetasRad = new List<float>();
 
         // Start is called before the first frame update
         void Start()
         {
             foreach(var l in LinkParams)
             {
-                deltaThetasDeg.Add(0);
+                deltaThetasRad.Add(0);
             }
             GetHTM();
         }
@@ -38,12 +38,15 @@ namespace RobotSimulation
             var returns = new List<Matrix4x4>();
             HTM = Matrix4x4.identity;
             int i = 0;
-            foreach (var p in LinkParams)
+            foreach (var param in LinkParams)
             {
-                var aMat = Matrix4x4.Translate(new Vector3(p.a, 0, 0));
-                var alphaMat = Matrix4x4.Rotate(Quaternion.Euler(p.alphaPI * Mathf.PI * Mathf.Rad2Deg, 0, 0));
-                var dMat = Matrix4x4.Translate(new Vector3(0, 0, p.d));
-                var thetaMat = Matrix4x4.Rotate(Quaternion.Euler(0, 0, (p.thetaPI * Mathf.PI + deltaThetasDeg[i]) * Mathf.Rad2Deg));
+                var aMat = Matrix4x4.Translate(new Vector3(param.a, 0, 0));
+                var alphaMat = Matrix4x4.Rotate(Quaternion.Euler(param.alphaPI * Mathf.PI * Mathf.Rad2Deg, 0, 0));
+                var dMat = Matrix4x4.Translate(new Vector3(0, 0, param.d));
+                var thetaMat = Matrix4x4.Rotate(Quaternion.Euler(0, 0, (param.thetaPI * Mathf.PI + deltaThetasRad[i]) * Mathf.Rad2Deg));
+                if (i == 1) { print("i:1 theta+pi/2 "+ ((param.thetaPI * Mathf.PI + deltaThetasRad[i]) * Mathf.Rad2Deg) + " theta2 " + (deltaThetasRad[i] * Mathf.Rad2Deg)); }
+                print($"i: {i}\na\n{aMat.ToString()}\nalpha\n{alphaMat.ToString()}\nd\n{dMat.ToString()}\ntheta\n{thetaMat.ToString()}");
+
                 HTM = HTM * aMat * alphaMat * dMat * thetaMat;
                 returns.Add(HTM);
                 i++;
@@ -59,10 +62,10 @@ namespace RobotSimulation
 
         public List<Matrix4x4> GetHTM(List<float> thetas)
         {
-            if(thetas.Count() == LinkParams.Count())
-            {
-                deltaThetasDeg = thetas;
-            }
+            //if(thetas.Count() == LinkParams.Count())
+            //{
+                deltaThetasRad = thetas;
+            //}
             return GetHTM();
         }
     }
